adf8bb9dd8b941872011caedb6851de33bcb8163
mbranting <mckenna.branting@gmail.com>
2018-06-15 22:55:22 +0000

    automatically backed up by learn

---
+++ apple.rb
@@ -0,0 +1,13 @@
+def fizzbuzz
+  number = gets.chomp.to_i
+  if number %3==0 && number %5==0
+    puts "FizzBuzz"
+  elsif number %3==0 
+    puts "Fizz"
+  elsif number %5==0
+    puts "Buzz"
+  else 
+    puts "nil"
+  end
+end
+Fizzbuzz
